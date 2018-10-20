3a1eb497fc098ce59590966c6a68441edc6c9a87
Derek Jensen <derek@deek.io>
2018-10-20 16:00:37 -0700

    moveVideo annoyances

--- src/scripts.js
+++ src/scripts.js
@@ -2,24 +2,57 @@ var recentPulse = 0;
 var recentScare = 0;
 var shakeInterval;
 
-const offset = 50;
-mixer.display.moveVideo({
-  top: offset,
-  bottom: offset + offset + 10,
-  left: offset,
-  right: offset
-});
-
 window.addEventListener('load', function initMixer() {
   var elHeartRate = document.getElementById('heart-rate');
   var elScares = document.getElementById('heart-rate-scare-number');
   var elGhost = document.getElementById('ghost-image');
   var elGhostMsg = document.getElementById('ghost-message');
+  var elToaster = document.getElementById('toaster');
   var scares = 0;
 
+  var interval;
+  const shakeit = function(element) {
+    element.style.display = 'block';
+    const STILL_POINT = 0;
+    var x = -1;
+    interval = setInterval(function() {
+      if (x == -1) {
+        element.style.transform = `rotate(${STILL_POINT}deg)`;
+      } else {
+        switch (x) {
+          case 0:
+            element.style.transform = `rotate(${STILL_POINT + 15}deg)`;
+            break;
+          case 1:
+            element.style.transform = `rotate(${STILL_POINT - 5}deg)`;
+            break;
+          case 2:
+            element.style.transform = `rotate(${STILL_POINT - 5}deg)`;
+            break;
+          default:
+            element.style.transform = `rotate(${STILL_POINT + 5}deg)`;
+            clearInterval(interval);
+        }
+      }
+      x++;
+    }, 50);
+  };
   mixer.display.position().subscribe(handleVideoResized);
 
   mixer.socket.on('onControlUpdate', handleControlUpdate);
+  mixer.socket.on('onSceneCreate', handleSceneCreate);
+
+  const offset = 50;
+  const position = {
+    top: offset,
+    bottom: offset + offset + 10,
+    left: offset,
+    right: offset
+  };
+  mixer.display.moveVideo(position);
+  setTimeout(function() {
+    mixer.display.moveVideo(position);
+  }, 500);
 
   shakeit(elGhost);
   shakeInterval = setInterval(function() {
@@ -29,7 +62,7 @@ window.addEventListener('load', function initMixer() {
   elGhost.addEventListener('mouseover', function(ev) {
     clearInterval(shakeInterval);
     elGhost.style = '';
-    elGhost.setAttribute('class', 'hover');
+    // elGhost.setAttribute('class', 'hover');
     elGhostMsg.setAttribute('class', 'hover');
   });
 
@@ -37,10 +70,10 @@ window.addEventListener('load', function initMixer() {
     shakeInterval = setInterval(function() {
       shakeit(elGhost);
     }, 5000);
-    elGhost.setAttribute('class', 'hoverout');
-    setTimeout(function() {
-      elGhost.setAttribute('class', '');
-    }, 500);
+    // elGhost.setAttribute('class', 'hoverout');
+    // setTimeout(function() {
+    //   elGhost.setAttribute('class', '');
+    // }, 500);
     elGhostMsg.setAttribute('class', '');
   });
 
@@ -50,15 +83,6 @@ window.addEventListener('load', function initMixer() {
       event: 'click'
     });
   };
-
-  // Move the video by a static offset amount
-  const offset = 50;
-  mixer.display.moveVideo({
-    top: offset,
-    bottom: offset + offset + 10,
-    left: offset,
-    right: offset
-  });
 
   Chart.defaults.global.tooltips = false;
 
@@ -70,7 +94,7 @@ window.addEventListener('load', function initMixer() {
         {
           label: '',
           data: [],
-          borderColor: 'green',
+          borderColor: 'red',
           fill: false,
           lineTension: 0.1
         }
@@ -118,57 +142,59 @@ window.addEventListener('load', function initMixer() {
     }
   });
 
-  mixer.isLoaded();
-});
-
-function handleVideoResized(position) {
-  const overlay = document.getElementById('spook-panel');
-  var ghost = document.getElementById('ghost');
-  const player = position.connectedPlayer;
-  overlay.style.position = 'absolute';
-  overlay.style.top = `${player.top + player.height}px`;
-  overlay.style.left = `${player.left}px`;
-  overlay.style.width = `${player.width - 20}px`;
-  ghost.style.position = 'absolute';
-  ghost.style.top = `${player.top}px`;
-  ghost.style.left = `${player.left}px`;
-  ghost.style.width = `${player.width}px`;
-  ghost.style.height = `${player.height}px`;
-}
-
-function handleControlUpdate(update) {
-  const control = update.controls[0];
-  if (control.controlID === 'pulse') {
-    recentPulse = control.meta.pulse.value;
-  } else {
-    recentScare = control.meta.scares.value;
-    elScares.innerText = recentScare;
+  function handleVideoResized(position) {
+    console.log('video resized', position);
+    const overlay = document.getElementById('spook-panel');
+    const ghost = document.getElementById('ghost');
+    const player = position.connectedPlayer;
+    overlay.style.position = 'absolute';
+    overlay.style.top = `${player.top + player.height}px`;
+    overlay.style.left = `${player.left}px`;
+    overlay.style.width = `${player.width - 20}px`;
+    ghost.style.position = 'absolute';
+    ghost.style.top = `${player.top}px`;
+    ghost.style.left = `${player.left}px`;
+    ghost.style.width = `${player.width}px`;
+    ghost.style.height = `${player.height}px`;
   }
-}
 
-var interval;
-shakeit = function(element) {
-  element.style.display = 'block';
-  var x = -1;
-  interval = setInterval(function() {
-    if (x == -1) {
-      element.style.transform = 'rotate(-160deg)';
-    } else {
-      switch (x) {
-        case 0:
-          element.style.transform = 'rotate(-145deg)';
-          break;
-        case 1:
-          element.style.transform = 'rotate(-165deg)';
-          break;
-        case 2:
-          element.style.transform = 'rotate(-165deg)';
-          break;
-        default:
-          element.style.transform = 'rotate(-155deg)';
-          clearInterval(interval);
+  function handleControlUpdate(update) {
+    const control = update.controls[0];
+    switch(control.controlID) {
+      case 'pulse':
+        recentPulse = control.meta.pulse.value;
+        break;
+      case 'scares':
+        recentScare = control.meta.scares.value;
+        elScares.innerText = recentScare;
+        break;
+      case 'toaster':
+        elToaster.innerText = control.meta.toaster.value;
+        popToast();
+        break;
+      default:
+        break;
+    }
+  }
+
+  function handleSceneCreate(data) {
+    const scenes = data.scenes;
+    if (scenes[0]) {
+      const scene = scenes[0];
+      const scaresControl = scene.controls.find(function (c) { return c.controlID === 'scares' });
+      console.log(scaresControl);
+      if (scaresControl && scaresControl.meta && scaresControl.meta.scares) {
+        elScares.innerText = scaresControl.meta.scares.value;
       }
     }
-    x++;
-  }, 50);
-};
+  }
+
+  function popToast() {
+    elToaster.style.top = `${elToaster.offsetTop + 120}px`;
+    setTimeout(function () {
+      elToaster.style.top = `${elToaster.offsetTop - 120}px`;
+    }, 3000);
+  }
+
+  mixer.isLoaded();
+});
