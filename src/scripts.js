36f73377ef871cce23ac81453a08ae38855da5c6
Derek Jensen <derek@deek.io>
2018-10-20 14:21:53 -0700

    minor updates

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,14 @@
 var recentPulse = 0;
+var recentScare = 0;
 var shakeInterval;
+
+const offset = 50;
+mixer.display.moveVideo({
+  top: offset,
+  bottom: offset + offset + 10,
+  left: offset,
+  right: offset
+});
 
 window.addEventListener('load', function initMixer() {
   var elHeartRate = document.getElementById('heart-rate');
@@ -13,27 +22,34 @@ window.addEventListener('load', function initMixer() {
   mixer.socket.on('onControlUpdate', handleControlUpdate);
 
   shakeit(elGhost);
-  shakeInterval = setInterval(function () {
+  shakeInterval = setInterval(function() {
     shakeit(elGhost);
   }, 5000);
 
-  elGhost.addEventListener('mouseover', function (ev) {
+  elGhost.addEventListener('mouseover', function(ev) {
     clearInterval(shakeInterval);
     elGhost.style = '';
     elGhost.setAttribute('class', 'hover');
     elGhostMsg.setAttribute('class', 'hover');
   });
 
-  elGhost.addEventListener('mouseout', function (ev) {
-    shakeInterval = setInterval(function () {
+  elGhost.addEventListener('mouseout', function(ev) {
+    shakeInterval = setInterval(function() {
       shakeit(elGhost);
     }, 5000);
     elGhost.setAttribute('class', 'hoverout');
-    setTimeout(function () {
+    setTimeout(function() {
       elGhost.setAttribute('class', '');
     }, 500);
     elGhostMsg.setAttribute('class', '');
   });
+
+  elGhost.onclick = function(ev) {
+    mixer.socket.call('giveInput', {
+      controlID: 'scare',
+      event: 'click'
+    });
+  };
 
   // Move the video by a static offset amount
   const offset = 50;
@@ -41,53 +57,56 @@ window.addEventListener('load', function initMixer() {
     top: offset,
     bottom: offset + offset + 10,
     left: offset,
-    right: offset,
+    right: offset
   });
 
   Chart.defaults.global.tooltips = false;
 
-  var ctx = document.getElementById("pulse").getContext('2d');
+  var ctx = document.getElementById('pulse').getContext('2d');
   var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-      datasets: [{
-        label: "",
-        data: [],
-        borderColor: 'green',
-        fill: false,
-        lineTension: 0.1
-      }]
+      datasets: [
+        {
+          label: '',
+          data: [],
+          borderColor: 'green',
+          fill: false,
+          lineTension: 0.1
+        }
+      ]
     },
     options: {
       legend: {
         display: false
       },
       scales: {
-        yAxes: [{
-          ticks: {
-            min: 60
+        yAxes: [
+          {
+            ticks: {
+              min: 60
+            }
           }
-        }],
-        xAxes: [{
-          type: 'realtime',
-          // position: 'bottom',
-          display: false
-        }]
+        ],
+        xAxes: [
+          {
+            type: 'realtime',
+            // position: 'bottom',
+            display: false
+          }
+        ]
       },
       plugins: {
-        streaming: { // enabled by default
+        streaming: {
+          // enabled by default
           duration: 20000, // data in the past 20000 ms will be displayed
           refresh: 1000, // onRefresh callback will be called every 1000 ms
           delay: 1000, // delay of 1000 ms, so upcoming values are known before plotting a line
           frameRate: 30, // chart is drawn 30 times every second
 
           // a callback to update datasets
-          onRefresh: function (chart) {
+          onRefresh: function(chart) {
             const newPulse = recentPulse;
-            if (newPulse > 130) {
-              scares++;
-              elScares.innerText = scares;
-            }
             elHeartRate.innerText = newPulse.toFixed(0);
             chart.data.datasets[0].data.push({
               x: Date.now(),
@@ -117,38 +136,39 @@ function handleVideoResized(position) {
   ghost.style.height = `${player.height}px`;
 }
 
-function handleControlUpdate (update) {
+function handleControlUpdate(update) {
   const control = update.controls[0];
   if (control.controlID === 'pulse') {
     recentPulse = control.meta.pulse.value;
   } else {
     recentScare = control.meta.scares.value;
+    elScares.innerText = recentScare;
   }
 }
 
 var interval;
-shakeit = function (element) {
-  element.style.display = "block";
+shakeit = function(element) {
+  element.style.display = 'block';
   var x = -1;
-  interval = setInterval(function () {
+  interval = setInterval(function() {
     if (x == -1) {
-      element.style.transform = "rotate(-160deg)"
+      element.style.transform = 'rotate(-160deg)';
     } else {
       switch (x) {
         case 0:
-          element.style.transform = "rotate(-145deg)";
+          element.style.transform = 'rotate(-145deg)';
           break;
         case 1:
-          element.style.transform = "rotate(-165deg)";
+          element.style.transform = 'rotate(-165deg)';
           break;
         case 2:
-          element.style.transform = "rotate(-165deg)"
+          element.style.transform = 'rotate(-165deg)';
           break;
         default:
-          element.style.transform = "rotate(-155deg)"
+          element.style.transform = 'rotate(-155deg)';
           clearInterval(interval);
       }
     }
     x++;
-  }, 50)
-}
+  }, 50);
+};
